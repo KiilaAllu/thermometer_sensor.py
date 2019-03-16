9da8546fe4f0ccd55764ba74ecfaec2b958376ab
Raspberry_1 <o.alvari.sivula@gmail.com>
2019-03-16 17:16:06 +0200

    lisätty muokkauksia tietokantaa varten

--- thermometer_sensor.py
+++ thermometer_sensor.py
@@ -1,6 +1,12 @@
 import os
 import glob
 import time
+import datetime
+import MySQLdb
+from time import strftime
+
+db = MySQLdb.connect(host="localhost", user="alvari", passwd="alvari", db="sensor") # replace password with your password
+cur = db.cursor()
 
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
@@ -8,6 +14,11 @@ os.system('modprobe w1-therm')
 base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
+
+def dateTime():
+	secs = float(time.time())
+	secs = secs*1000
+	return secs
 
 def read_temp_raw():
     f = open(device_file, 'r')
@@ -26,6 +37,24 @@ def read_temp():
         temp_c = float(temp_string) / 1000.0
         return temp_c
 
-while True:
+secs = dateTime()
+temperature = read_temp()
+
+try:
 	print(read_temp())
+	print "Writing to the database..."
+	cur.execute(*sql)
+	db.commit()
+	print "Write complete"
+
 	time.sleep(1)
+
+except:
+	db.rollback()
+	print "We have a problem"
+
+cur.close()
+db.close()
+
+print secs
+print temperature
